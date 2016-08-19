4cb2aeab42f7473fb48c5e130bc6b7dca1ec39da
Strong Liu <stliu@hibernate.org>
2016-08-19 10:12:34 +0800

    first init

---
+++ src/main/java/im/baas/sandbox/IgniteApplication.java
@@ -0,0 +1,12 @@
+package im.baas.sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgniteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgniteApplication.class, args);
+	}
+}
